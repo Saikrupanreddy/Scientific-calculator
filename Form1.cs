4ed8d620e1b775852fa915bf9e81a29d7b4f0ca0
Saikrupanreddy <115462027+Saikrupanreddy@users.noreply.github.com>
2023-04-12 16:43:31 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scientific_calculator
+{
+    public partial class Form1 : Form
+    {
+        double enterFirstValue, enterSecondValue;
+        String op;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterNumbers(object sender, EventArgs e)
+        {
+            Button num = (Button)sender;
+
+            if (txtResult.Text == "0")
+                txtResult.Text = "";
+            {
+                if(num.Text ==".")
+                {
+                    if(!txtResult.Text.Contains("."))
+                        txtResult.Text = txtResult.Text + num.Text;
+                }
+                else
+                {
+                    txtResult.Text = txtResult.Text + num.Text;
+                }
+            }
+        }
+
+        private void numOperators(object sender, EventArgs e)
+        {
+            Button num = (Button)sender;
+            enterFirstValue = Convert.ToDouble(txtResult.Text);
+            op = num.Text;
+            txtResult.Text = "";
+
+
+        }
+
+        private void btnequals_Click(object sender, EventArgs e)
+        {
+            enterSecondValue = Convert.ToDouble(txtResult.Text);
+
+            switch (op)
+            {
+                case "+":
+                    txtResult.Text = (enterFirstValue + enterSecondValue).ToString();
+                    break;
+
+                case "-":
+                    txtResult.Text = (enterFirstValue - enterSecondValue).ToString();
+                    break;
+
+                case "*":
+                    txtResult.Text = (enterFirstValue * enterSecondValue).ToString();
+                    break;
+
+                case "/":
+                    txtResult.Text = (enterFirstValue / enterSecondValue).ToString();
+                    break;
+
+                case "Mod":
+                    txtResult.Text = (enterFirstValue % enterSecondValue).ToString();
+                    break;
+
+                case "Exp":
+                    double i = Convert.ToDouble(txtResult.Text);
+                    double j;
+                    j = enterSecondValue;
+                    txtResult.Text = Math.Exp(i  *  Math.Log(j*4)).ToString();
+                    break;
+
+                default: 
+                    break;
+            }
+        }
+
+        private void btnClearEntry_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "0";
+
+            String f, s;
+            f = Convert.ToString(enterFirstValue);
+            s = Convert.ToString(enterSecondValue);
+
+            f = "";
+            s = "";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "0";
+        }
+
+        private void btnPM_Click(object sender, EventArgs e)
+        {
+            double q = Convert.ToDouble(txtResult.Text);
+            txtResult.Text = Convert.ToString(-1*q);
+        }
+
+        private void btnBS_Click(object sender, EventArgs e)
+        {
+            if(txtResult.Text.Length > 0)
+            {
+                txtResult.Text = txtResult.Text.Remove(txtResult.Text.Length - 1,1);
+            }
+            if (txtResult.Text == "")
+            {
+                txtResult.Text = "0";
+            }
+        }
+
+        private void standardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Width = 300; //816
+            txtResult.Width = 260;//350
+
+        }
+
+        private void scientificToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Width = 570; 
+            txtResult.Width = 537;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult exitCal;
+            exitCal = MessageBox.Show("Confirm if you want to exit", "Scientific Calculator",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(exitCal == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void btnPI_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = "3.141592653589976323";
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            double logg = Convert.ToDouble(txtResult.Text);
+            logg = Math.Log10(logg);
+            txtResult.Text = Convert.ToString(logg);
+        }
+
+        private void btnsq_click(object sender, EventArgs e)
+        {
+            double sqrt = Convert.ToDouble(txtResult.Text);
+            sqrt = Math.Sqrt(sqrt);
+            txtResult.Text = Convert.ToString(sqrt);
+
+        }
+
+        private void btnx2(object sender, EventArgs e)
+        {
+            double x;
+            x = Convert.ToDouble(txtResult.Text) * Convert.ToDouble(txtResult.Text);
+            txtResult.Text =Convert.ToString(x);
+        }
+
+        private void btnx3(object sender, EventArgs e)
+        {
+            double x,q,p,m;
+           q = Convert.ToDouble(txtResult.Text);
+           p = Convert.ToDouble(txtResult.Text);
+           m = Convert.ToDouble(txtResult.Text);
+
+            x = (q * p * m);
+            txtResult.Text = Convert.ToString(x);
+        }
+
+        private void btn1x(object sender, EventArgs e)
+        {
+            double a;
+
+            a = Convert.ToDouble(1.0/ Convert.ToDouble(txtResult.Text));
+            txtResult.Text = Convert.ToString(a);
+        }
+
+        private void btnlnx(object sender, EventArgs e)
+        {
+            double lnx = Convert.ToDouble(txtResult.Text);
+            lnx = Math.Log(lnx);
+            txtResult.Text = Convert.ToString(lnx);
+        }
+
+        private void btnperc(object sender, EventArgs e)
+        {
+            double a;
+
+            a = Convert.ToDouble(txtResult.Text) / Convert.ToDouble(100);
+            txtResult.Text = Convert.ToString(a);
+        }
+
+        private void btnOct(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtResult.Text);
+            txtResult.Text = Convert.ToString(a, 8);
+        }
+
+        private void btnTanh(object sender, EventArgs e)
+        {
+            double Tanh = Convert.ToDouble(txtResult.Text);
+            Tanh = Math.Tanh(Tanh);
+            txtResult.Text = Convert.ToString(Tanh);
+        }
+
+        private void btnTan(object sender, EventArgs e)
+        {
+            double Tan = Convert.ToDouble(txtResult.Text);
+            Tan = Math.Tan(Tan);
+            txtResult.Text = Convert.ToString(Tan);
+        }
+
+        private void btnHex(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtResult.Text);
+            txtResult.Text = Convert.ToString(a, 16);
+        }
+
+        private void btnBin(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtResult.Text);
+            txtResult.Text = Convert.ToString(a, 2);
+        }
+
+        private void btnCos(object sender, EventArgs e)
+        {
+            double Cos = Convert.ToDouble(txtResult.Text);
+            Cos = Math.Cos(Cos);
+            txtResult.Text = Convert.ToString(Cos);
+        }
+
+        private void btnCosh(object sender, EventArgs e)
+        {
+            double Cosh = Convert.ToDouble(txtResult.Text);
+            Cosh = Math.Cosh(Cosh);
+            txtResult.Text = Convert.ToString(Cosh);
+        }
+
+        private void btnDec(object sender, EventArgs e)
+        {
+            double dec = Convert.ToDouble(txtResult.Text);
+            int i1 = Convert.ToInt32(dec);
+            int i2 = (int)dec;
+            txtResult.Text = Convert.ToString(i2);
+        }
+
+        private void btnSin(object sender, EventArgs e)
+        {
+            double sin = Convert.ToDouble(txtResult.Text);
+            sin = Math.Sin(sin);
+            txtResult.Text = Convert.ToString(sin);
+        }
+
+        private void btnSinh(object sender, EventArgs e)
+        {
+            double sh = Convert.ToDouble(txtResult.Text);
+            sh = Math.Sinh(sh);
+            txtResult.Text = Convert.ToString(sh);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 300; //816
+            txtResult.Width = 260;//350
+        }
+    }
+}
